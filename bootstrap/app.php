c47861b696f71aa2dae59c328fd6ee80392abc1e
Anatoly Frolov (anafro) <anatolyfroloff@gmail.com>
2026-01-28 11:53:05 +1000

    chore(healthcheck): change health route to /status
    ...to satisfy `php-fpm-healthcheck` utility

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,9 +8,9 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/status',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [

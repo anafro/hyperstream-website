d33e76f7feba8a8390ec047273b381a859ef64e1
Anatoly Frolov (anafro) <anatolyfroloff@gmail.com>
2026-03-06 19:32:10 +1000

    fix(frontend): make audio play on play button click in playlist

--- resources/js/components/playlists/PlayerTimeline.vue
+++ resources/js/components/playlists/PlayerTimeline.vue
@@ -18,6 +18,7 @@ const {
 <template>
     <div
         class="fixed bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-zinc-100 from-75% to-transparent to-100% dark:from-zinc-950 flex items-center justify-center gap-x-12 mx-12">
-        <audio class="w-full bg-zinc-100 dark:bg-zinc-950" controls v-if="songUri !== null" :src="songUri"></audio>
+        <audio class="w-full bg-zinc-100 dark:bg-zinc-950" autoplay controls v-if="songUri !== null"
+            :src="songUri"></audio>
     </div>
 </template>

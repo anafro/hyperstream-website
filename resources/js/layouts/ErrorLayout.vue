4eb2eff11b9d8532fa5cbef6d084018dbe1dad8c
Anatoly Frolov (anafro) <anatolyfroloff@gmail.com>
2026-03-06 12:21:12 +1000

    fix(frontend): add missing error layout

---
+++ resources/js/layouts/ErrorLayout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import LayoutBase from "@/layouts/LayoutBase.vue";
+</script>
+
+<template>
+<LayoutBase>
+    <div class="flex items-center justify-center h-svh">
+        <slot></slot>
+    </div>
+</LayoutBase>
+</template>

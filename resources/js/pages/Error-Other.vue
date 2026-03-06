c4af7fb4c938c44795af4b1d0bfd0961d4c82a08
Anatoly Frolov (anafro) <anatolyfroloff@gmail.com>
2026-03-06 12:18:08 +1000

    feat(frontend): add error pages

---
+++ resources/js/pages/Error-Other.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ErrorLayout from "@/layouts/ErrorLayout.vue";
+import Logotype from "@/components/shared/Logotype.vue";
+import Text from "@/components/shared/Text.vue";
+</script>
+
+<template>
+<ErrorLayout>
+    <div class="flex flex-col gap-y-4">
+        <Logotype variant="logo"></Logotype>
+        <Text class="text-center">
+            Something went wrong.
+        </Text>
+    </div>
+</ErrorLayout>
+</template>

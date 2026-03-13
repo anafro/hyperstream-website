d35621b0f8a0134315ec25582fe0430b8bfd2a94
Anatoly Frolov (anafro) <anatolyfroloff@gmail.com>
2026-03-13 17:21:53 +1000

    chore(events): add more events

--- resources/js/hyperstream/events/events.ts
+++ resources/js/hyperstream/events/events.ts
@@ -1,4 +1,7 @@
 import { type Song } from "@/hyperstream/models/song";
+import { EffectType } from "@/hyperstream/models/effect-type";
+import { Remix } from "@/hyperstream/models/remix";
+import { Rack } from "@/hyperstream/models/rack";
 
 export interface EventHeader {
     happenedAt: number;
@@ -29,6 +32,35 @@ export type SongListed = Event<{
     songs: Song[],
 }>;
 
+export type SongFind = Event<Pick<Song, 'id'>>;
+export type SongFound = Event<Song>;
+
+export type SongRemix = Event<{ songId: number, effects: Rack, name: string }>;
+export type SongRemixed = {
+    songId: number;
+};
+
+
+export type EffectList = Signal;
+export type EffectListed = Event<{
+    effects: EffectType[],
+}>;
+
+export type RemixList = Event<{
+    songId?: number,
+}>;
+export type RemixListed = Event<{
+    songId?: number,
+    remixes: Remix[],
+}>;
+
+export type RemixExpose = Event<{ id: number }>;
+export type RemixExposed = Event<{
+    id: number;
+    uri: string;
+}>;
+
+
 export interface HyperstreamEvents {
     "song.download": SongDownload;
     "song.downloaded": SongDownloaded;
@@ -36,4 +68,14 @@ export interface HyperstreamEvents {
     "song.exposed": SongExposed;
     "song.list": SongList;
     "song.listed": SongListed;
+    "song.find": SongFind;
+    "song.found": SongFound;
+    "song.remix": SongRemix;
+    "song.remixed": SongRemixed;
+    "effect.list": EffectList;
+    "effect.listed": EffectListed;
+    "remix.list": RemixList;
+    "remix.listed": RemixListed;
+    "remix.expose": RemixExpose;
+    "remix.exposed": RemixExposed;
 }
